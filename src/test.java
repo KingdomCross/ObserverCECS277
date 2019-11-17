4caff762bea1cf5df723e434cf6ab1476721be2b
KingdomCross <alexchheng1998@yahoo.com>
2019-11-16 20:19:51 -0800

    Further Test

--- src/test.java
+++ src/test.java
@@ -2,5 +2,8 @@ public class test {
     public static void main(String[] args) {
         Stock stock = new Stock();
         stock.notifyObservers();
+        System.out.println("Hello Kitty");
+        Trader trader = new Trader(stock);
+        trader.display();
     }
 }
