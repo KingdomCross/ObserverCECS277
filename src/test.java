3fa5b9a0f94dd625f6660545a4b4b0a91dc36098
Chloe Culver <c.culver831@gmail.com>
2019-11-18 17:38:24 -0800

    Commenting and clean up. Have not finished the Oberver

--- src/test.java
+++ src/test.java
@@ -5,33 +5,33 @@
  */
 public class test {
     public static void main(String[] args) {
-        Stock stock = new Stock(234, "COCA");
-        Stock s2 = new Stock(150, "APPL");
-        Stock s3 = new Stock(567, "GOOG");
-        Trader Hanson = new Trader(stock, "Hanson");
-        Trader Dylan = new Trader(s2, "Dylan");
-        Trader Joe = new Trader(s3, "Joe");
-        //adding Traders to Stocks
-        s3.registerObserver(Hanson);
-        s3.registerObserver(Dylan);
-      //adding Traders to Stocks
-        s2.registerObserver(Joe);
-      //adding Traders to Stocks
-        stock.registerObserver(Hanson);
-        stock.registerObserver(Dylan);
-        stock.registerObserver(Joe);
+        Stock COCO = new Stock(234, "COCA");
+        Stock APPL = new Stock(150, "APPL");
+        Stock GOOG = new Stock(567, "GOOG");
+        Trader Hanson = new Trader(COCO, "Hanson");
+        Trader Dylan = new Trader(GOOG, "Dylan");
+        Trader Joe = new Trader(APPL, "Joe");
+        //adding Traders to COCOs
+        APPL.registerObserver(Hanson);
+        APPL.registerObserver(Dylan);
+      //adding Traders to COCOs
+        GOOG.registerObserver(Joe);
+      //adding Traders to COCOs
+        COCO.registerObserver(Hanson);
+        COCO.registerObserver(Dylan);
+        COCO.registerObserver(Joe);
         
-        System.out.println(".....Trading Stocks.....");
+        System.out.println(".....Trading COCOs.....");
         Hanson.sell();
-        stock.removeObserver(Hanson);
+        COCO.removeObserver(Hanson);
         Dylan.sell();
-        s2.removeObserver(Dylan);
+        GOOG.removeObserver(Dylan);
         Joe.buy();
         Joe.sell();
-        stock.removeObserver(Joe);
-        stock.notifyObservers();
-        s2.notifyObservers();
-        s3.notifyObservers();
+        COCO.removeObserver(Joe);
+        COCO.notifyObservers();
+        GOOG.notifyObservers();
+        APPL.notifyObservers();
 
     }
 }
